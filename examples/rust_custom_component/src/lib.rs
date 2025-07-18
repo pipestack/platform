bb77e5e2ae3a99f145f997163449d2990d50a909
@mootoday <154029656+mootoday@users.noreply.github.com>
2025-07-18 15:45:48 -0700

    Add the rust_custom_component example

---
+++ examples/rust_custom_component/src/lib.rs
@@ -0,0 +1,14 @@
+use crate::{exports::pipestack::customer::customer::Guest, wrpc::rpc};
+
+wit_bindgen::generate!({ generate_all });
+
+struct Component;
+
+impl Guest for Component {
+    fn run(input: String) -> Result<String, rpc::error::Error> {
+        // Ok(input.to_uppercase())
+        Ok(input.to_lowercase())
+    }
+}
+
+export!(Component);
