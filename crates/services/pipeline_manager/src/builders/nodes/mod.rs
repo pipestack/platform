7ef46ca55073df9cfc91243e32ae7e4f86b4fb6b
@mootoday <154029656+mootoday@users.noreply.github.com>
2025-10-05 13:50:37 -0700

    Update node versions

--- crates/services/pipeline_manager/src/builders/nodes/mod.rs
+++ crates/services/pipeline_manager/src/builders/nodes/mod.rs
@@ -3,7 +3,7 @@ pub mod out;
 pub mod processor;
 pub mod registry;
 
-pub const NODE_VERSION_IN_HTTP: &str = "0.1.5";
+pub const NODE_VERSION_IN_HTTP: &str = "0.1.6";
 pub const NODE_VERSION_IN_INTERNAL: &str = "0.1.7";
 pub const NODE_VERSION_OUT_HTTP_WEBHOOK: &str = "0.1.6";
 pub const NODE_VERSION_OUT_INTERNAL: &str = "0.1.6";
